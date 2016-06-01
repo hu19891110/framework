d8ba9c2ae35fd3264dc7d0af001123ee65695f82
Wellming Li <wellming.li@howzhi.com>
2016-06-01 16:42:59 +0800

    init unittest.

---
+++ tests/FrameworkTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Footstones\RPC\Tests;
+
+use Footstones\RPC\Server;
+use Footstones\RPC\Packager;
+use Footstones\RPC\Consts;
+use Footstones\RPC\Tests\TestService;
+use Footstones\RPC\Client;
+
+class FrameworkTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInt()
+    {
+        $this->assertTrue(true);
+    }
+
+}
